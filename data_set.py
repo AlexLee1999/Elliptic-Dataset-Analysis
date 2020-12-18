0a322734b00acc77b49d1d379113b4d6279283bb
alexlee0603 <alexlee0603alex@gmail.com>
2020-12-18 10:38:28 +0800

    fix datatype

--- data_set.py
+++ data_set.py
@@ -3,13 +3,12 @@
 
 import numpy as np
 import csv
-file = '../elliptic_bitcoin_dataset/elliptic_txs_features.csv'
+file_features = '../elliptic_bitcoin_dataset/elliptic_txs_features.csv'
 file_class = '../elliptic_bitcoin_dataset/elliptic_txs_classes.csv'
-def read_file():
-    reader = csv.reader(open(file, "r"), delimiter=",")
-    x = list(reader)
-    results = np.array(x).astype("float")
-    return results
+def read_features_file():
+    with open(file_features) as f:
+        FH = np.genfromtxt(f, delimiter=',', dtype='str')
+    return FH
 
 def read_file_class():
     with open(file_class) as f:
@@ -18,18 +17,17 @@ def read_file_class():
 
 def clear_unknown(features, classes):
     del_lst = []
-    count = 0
-    for i in range(1, len(features)):
+    for i in range(len(features)):
         if classes[i][1] == 'unknown':
-            del_lst.append(i-1)
-        else:
-            count += 1
+            del_lst.append(i)
+
     features = np.delete(features, del_lst, axis=0)
-    np.savetxt("clear.csv", features, delimiter=",")
-    print(count)
+    np.savetxt("clear_data.csv", features, delimiter=",",fmt="%s")
     
 if __name__ == "__main__":
     results_class = read_file_class()
-    results_features = read_file()
-    
+    results_features = read_features_file()
+    results_class = results_class[1::]
+    r = results_class[:,1:]
+    results_features = np.concatenate((r, results_features), axis=1)
     clear_unknown(results_features, results_class)
