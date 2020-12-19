827fc18b8a86f7a36338afdaf023e07a6758cf49
alexlee0603 <alexlee0603alex@gmail.com>
2020-12-19 18:15:29 +0800

    add MLP

--- orginizing_data_src/clear_unknown_data.py
+++ orginizing_data_src/clear_unknown_data.py
@@ -7,9 +7,10 @@ Usage :
 - Change file name if you need
 '''
 import numpy as np
-file_features = '../../elliptic_bitcoin_dataset/clear_data_test.csv'
+file_features = '../../elliptic_bitcoin_dataset/full_data.csv'
 file_class = '../../elliptic_bitcoin_dataset/elliptic_txs_classes.csv'
 clear_data_csv = "../../elliptic_bitcoin_dataset/clear_data.csv"
+unknown_data_csv = "../../elliptic_bitcoin_dataset/unknown_data.csv"
 def read_features_file():
     with open(file_features) as f:
         FH = np.genfromtxt(f, delimiter=',', dtype='str')
@@ -25,7 +26,9 @@ def clear_unknown(features, classes):
     for i in range(len(features)):
         if classes[i][1] == 'unknown':
             del_lst.append(i)
-
+    for i in range(len(del_lst)):
+        features[del_lst[i]][0] = '-1'
+    np.savetxt(unknown_data_csv, features[del_lst], delimiter=",",fmt="%s")
     features = np.delete(features, del_lst, axis=0)
     np.savetxt(clear_data_csv, features, delimiter=",",fmt="%s")
     
