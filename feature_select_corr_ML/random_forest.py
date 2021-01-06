79c6ec73348610153786579c34be10559b1c3a15
alexlee0603 <alexlee0603alex@gmail.com>
2021-01-06 23:40:20 +0800

    add results

---
+++ feature_select_corr_ML/random_forest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import confusion_matrix, classification_report
+from shared import prepare_data
+
+if __name__ == "__main__":
+    X_train, X_test, Y_train, Y_test = prepare_data()
+    clf = RandomForestClassifier(n_estimators=50, max_depth=100, random_state=0).fit(X_train, Y_train)
+    _predict = clf.predict(X_test)
+    cm = confusion_matrix(Y_test, _predict, labels=clf.classes_)
+    precision, recall, f1, _ = precision_recall_fscore_support(Y_test, _predict)
+    fi = open('./random_forest.txt', 'w')
+    fi.write(f"precision = {precision[1]}\n")
+    fi.write(f"recall = {recall[1]}\n")
+    fi.write(f"F1 = {f1[1]}\n")
+    fi.write(classification_report(Y_test, _predict))
+    fi.write(f"{cm}")
+    fi.close()
--- feature_select_corr_ML/shared.py
+++ feature_select_corr_ML/shared.py
@@ -23,5 +23,32 @@ def prepare_data():
     X = data[feature]
     Y = data['class']
     Y = Y.apply(lambda x: 0 if x == '2' else 1 )
+    cor = []
+    for i in range(171):
+        x = X[f'{i}'].corr(Y)
+        cor.append(x)
+    corr = X.corr()
+    columns = np.full((corr.shape[0],), True, dtype=bool)
+    for i in range(corr.shape[0]):
+        for j in range(corr.shape[0]):
+            if corr.iloc[i,j] >= 0.99:
+                if columns[j] and abs(cor[j]) < abs(cor[i]):
+                    if columns[i]:
+                        columns[j] = False
+                elif columns[i] and abs(cor[j]) > abs(cor[i]):
+                    if columns[j]:
+                        columns[i] = False
+    
+    fi = open('./deleted.txt', 'w')
+    for i in range(len(columns)):
+    
+        fi.write(f'{i} : {columns[i]}\n')
+    fi.close()
+        
+
+    selected_columns = X.columns[columns]
+    
+    X = X[selected_columns]
+
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3,random_state=0,shuffle=False)
     return X_train, X_test, Y_train, Y_test
