60f93cc09fe63724260cf6c6bfdc278e25405b59
alexrumrum <alexlee0603alex@gmail.com>
2021-01-16 21:37:18 +0800

    add check file in shell

--- start.sh
+++ start.sh
@@ -1,68 +1,76 @@
 #!/bin/bash
-
-FILE=../elliptic_bitcoin_dataset/full_data.csv
-if [[ -f "$FILE" ]]; then
-    echo "$FILE Exist"
+CLASSFILE=../elliptic_bitcoin_dataset/elliptic_txs_classes.csv
+FEATUREFILE=../elliptic_bitcoin_dataset/elliptic_txs_features.csv
+RESULTSFILE=../elliptic_bitcoin_dataset/Result.csv
+EDGEFILE=../elliptic_bitcoin_dataset/elliptic_txs_edgelist.csv
+if [[ -f "$CLASSFILE"  &&  -f "$FEATUREFILE"  &&  -f "$RESULTSFILE"  &&  -f "$EDGEFILE" ]]; then
+    echo "ALL Exist"
+    FILE=../elliptic_bitcoin_dataset/full_data.csv
+    if [[ -f "$FILE" ]]; then
+        echo "$FILE Exist"
+    else
+        echo "Adding Features"
+        cd orginizing_data_src/
+        python3 connect_real_txs.py
+        cd ..
+    fi
+    case $1 in
+        -all|-A)
+            cd raw_ML/
+            bash run_list.sh
+            cd ../modified_ML
+            bash run_list.sh
+            cd ../pca_ML
+            bash run_list.sh
+            cd ../feature_select_corr_ML
+            bash run_list.sh
+            cd ../feature_select_pca_ML/
+            bash run_list.sh
+            cd ../stat
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        -raw|-R)
+            cd raw_ML/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        -modified|-M)
+            cd modified_ML/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        -pca|-P)
+            cd pca_ML/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        pcaf|-PF)
+            cd feature_select_pca_ML/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        -corrf|-CF)
+            cd feature_select_corr_ML/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        -stat|-S)
+            cd stat/
+            bash run_list.sh
+            cd ..
+            echo "Finished $1"
+        ;;
+        *)
+            echo "Wrong Flag"
+        ;;
+    esac
 else
-    echo "Adding Features"
-    cd orginizing_data_src/
-    python3 connect_real_txs.py
-    cd ..
+    echo "Missing file"
 fi
-case $1 in
-    -all|-A)
-        cd raw_ML/
-        bash run_list.sh
-        cd ../modified_ML
-        bash run_list.sh
-        cd ../pca_ML
-        bash run_list.sh
-        cd ../feature_select_corr_ML
-        bash run_list.sh
-        cd ../feature_select_pca_ML/
-        bash run_list.sh
-        cd ../stat
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    -raw|-R)
-        cd raw_ML/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    -modified|-M)
-        cd modified_ML/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    -pca|-P)
-        cd pca_ML/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    pcaf|-PF)
-        cd feature_select_pca_ML/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    -corrf|-CF)
-        cd feature_select_corr_ML/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    -stat|-S)
-        cd stat/
-        bash run_list.sh
-        cd ..
-        echo "Finished $1"
-    ;;
-    *)
-        echo "Wrong Flag"
-    ;;
-esac
---
+++ stat/pca_plot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.decomposition import PCA
+features = pd.read_csv('../../elliptic_bitcoin_dataset/elliptic_txs_features.csv',header=None, dtype='float64')
+classes = pd.read_csv('../../elliptic_bitcoin_dataset/elliptic_txs_classes.csv')
+feature = [str(i) for i in range(165)]
+features.columns = ["txId","time_step"] + feature
+features = pd.merge(features,classes,left_on="txId",right_on="txId",how='left')
+features['class'] = features['class'].apply(lambda x: '0' if x == "unknown" else x)
+data = features[(features['class']=='1') | (features['class']=='2')]
+X = data[feature]
+Y = data['class']
+Y = Y.apply(lambda x: 0 if x == '2' else 1 )
+pca = PCA(n_components=2).fit(X) 
+pcaf = pca.transform(X)
+
+trans = pd.DataFrame()
+trans['x'] = pcaf[:,0]
+trans['y'] = pcaf[:,1]
+trans["illicit"] = Y
+
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+x="x", y="y",
+hue="illicit",
+palette=sns.color_palette("hls",2),
+data=trans,
+legend="full",
+alpha=0.3
+)
+
+plt.savefig('../image/llicit2D.png')
